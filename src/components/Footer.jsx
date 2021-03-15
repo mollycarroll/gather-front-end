54b124184a89c8af818e6c6e4aad4235d5aad976
jamera mcneil <j.mcneil04@gmail.com>
2021-03-15 16:07:59 -0400

    created Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer>
+                    <a href = 'https://github.com/JameraMcNeil/gather'>
+                    Completed by Jamera McNeil, Molly Carroll, and Jessica Gallagher
+                    </a>
+                </footer>
+            </div>
+        )
+    }
+}
+
+export default Header
