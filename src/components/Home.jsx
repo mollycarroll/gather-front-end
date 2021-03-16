3910e20cda92cbbda6868e48165cba747d7e77f7
jessicagallagher <jessicalyngallagher@me.com>
2021-03-16 19:22:49 -0400

    there goes myyyy herooo (on home.jsx)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,8 @@ export default class Home extends Component {
 			<div>
         <div className="container-fluid px-0 mb-5">
           <div className="hero">
-            <p className="text-center hero-text">Gather</p>
+            <p className="text-center hero-text mb-0 d-flex justify-content-center real">Gather</p>
+            <p className="text-center hero-text reflection d-flex justify-content-center">Gather</p>
           </div>
         </div>
 
