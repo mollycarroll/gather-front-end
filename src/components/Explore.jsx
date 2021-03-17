9f6b2538aaf058974b9095d749812afca76a0a83
jamera mcneil <j.mcneil04@gmail.com>
2021-03-17 19:37:38 -0400

    set up initial explore.jsx file

---
+++ src/components/Explore.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios'
+import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
+
+export default class Explore extends Component {
+    render() {
+        return (
+            <di></di>
+        )
+    }
+};
