17f5fffc58314fa352eae00acec83767d6631d87
jamera mcneil <j.mcneil04@gmail.com>
2021-03-17 19:46:01 -0400

    building out constructor and method for Explore file

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -3,9 +3,31 @@ import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom'
 
 export default class Explore extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            City: '',
+            State: '',
+            Activity: ''
+        }
+    }
+
+    componentDidMount() {
+		console.log(this.props.match.params.id)
+		console.log(this.state.redirect)
+		axios.get(`${baseURL}/cities/${this.props.match.params.id}`)
+			.then(response => {
+				this.setState({
+					City: response.data.City,
+					State: response.data.State,
+					Activity: response.data.Activity,
+				})
+			})
+	}
+    
     render() {
         return (
-            <di></di>
+            <div></div>
         )
     }
 };
