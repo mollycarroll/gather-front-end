8c549a2560573f64d6f9b751f4ba44ddf937ba6d
jessicagallagher <jessicalyngallagher@me.com>
2021-03-19 11:12:16 -0400

    still works

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -11,30 +11,7 @@ if(process.env.NODE_ENV === 'development') {
 	baseURL = 'https://pure-waters-81977.herokuapp.com'
 }
 
-export default class Explore extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cities: [],
-            someRandomCity: {
-                City: '',
-                State: '',
-                Activity: ''
-            },
-        }
-    }
-
-    getCities() {
-        axios.get(baseURL + '/cities')
-        .then(data => {
-            console.log(data)
-            this.setState({ cities: data.data}, () => {
-                this.randomCities()
-            })
-        })
-    }
-
-export default class Explore extends Component {
+class Explore extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
@@ -45,6 +22,16 @@ export default class Explore extends Component {
 				Activity: ''
 			},
 		}
+	}
+
+	getCities() {
+		axios.get(baseURL + '/cities')
+			.then(data => {
+				console.log(data)
+				this.setState({ cities: data.data }, () => {
+					this.randomCities()
+				})
+			})
 	}
 
 	getCities() {
@@ -99,6 +86,8 @@ export default class Explore extends Component {
 				<p>Activity: {this.state.someRandomCity.Activity} </p> */}
 
             </div>
-        )
-    }
+		)
+	}
 };
+
+export default Explore
